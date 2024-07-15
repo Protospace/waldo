563243aa039b32e7bc1664b61e24522596946c30
Tanner Collin <git@tannercollin.com>
2024-07-15 19:20:46 +0000

    Update index page

--- main.py
+++ main.py
@@ -131,7 +131,7 @@ async def new_message(event):
 
 
 async def index(request):
-    return web.Response(text='Hello, world')
+    return web.Response(text='<i>KEEP LOOKING...</i>')
 
 async def sms(request):
     post = await request.post()
